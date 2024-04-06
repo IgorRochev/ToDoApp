8be300e2d1447c20dd4a9cfbb24e082b3404725d
Rochev Igor <rochev12345@mail.ru>
2024-04-06 16:40:18 +0300

    add clear completed tasks button

---
+++ lib/views/components/clear_completed_tasks_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ClearCompletedTasksButton extends StatelessWidget {
+  const ClearCompletedTasksButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 6, left: 14, bottom: 0),
+      child: TextButton(
+          style: ButtonStyle(
+              textStyle: const MaterialStatePropertyAll(TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w400,
+              )),
+              overlayColor: MaterialStateProperty.all(Colors.transparent)),
+          onPressed: () => {},
+          child: const Text(
+            'Очистить',
+          )),
+    );
+  }
+}
--- lib/views/finished_to_do_list/finished_to_do_list.dart
+++ lib/views/finished_to_do_list/finished_to_do_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_app/view_models/tasks_lists_change_view_model.dart';
+import 'package:todo_list_app/views/components/clear_completed_tasks_button.dart';
 import 'package:todo_list_app/views/components/task.dart';
 
 class FinishedToDoList extends StatelessWidget {
@@ -15,9 +16,24 @@ class FinishedToDoList extends StatelessWidget {
       color: const Color(0xFFD6D7EF),
       child: ListView.builder(
         itemCount: finishedTasks.length,
-        padding: const EdgeInsets.only(top: 21),
         itemBuilder: (BuildContext context, int index) {
           final finishedTask = finishedTasks.values.elementAt(index);
+          if (index == 0) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ClearCompletedTasksButton(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2),
+                  child: Task(
+                    task: finishedTask,
+                    listIndex: listIndex,
+                    taskIndex: index,
+                  ),
+                )
+              ],
+            );
+          }
           return Task(
             task: finishedTask,
             listIndex: listIndex,
