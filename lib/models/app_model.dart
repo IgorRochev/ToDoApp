ccb1ee019835021c2d1f27117d9057ca3e90c77b
Rochev Igor <rochev12345@mail.ru>
2024-04-08 14:38:34 +0300

    to-do list app, version 1.0.1 (add func to clear completed tasks, bug fixes)

--- lib/models/app_model.dart
+++ lib/models/app_model.dart
@@ -1,5 +1,6 @@
 class AppModel {
-  DateTime toDoListDate = DateTime.now(); //дата списка задач
+  DateTime toDoListDate = DateTime(DateTime.now().year, DateTime.now().month,
+      DateTime.now().day, 0, 0, 0, 0, 0);
   int screenIndex = 0; //индекс вкладки на основной странице
   DateTime dateTimeNow = DateTime.now();
 }
